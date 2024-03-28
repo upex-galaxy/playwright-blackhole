de43aa66ee80d2de0e1b7a2111f18e1df66b4a4d
saiotest <elyermad@gmail.com>
2024-03-28 19:57:24 -0300

    ely-KT: dropdown options strategy select/random

--- tests/pages/SpaceDestinationPage.ts
+++ tests/pages/SpaceDestinationPage.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page, } from '@playwright/test';
+import { expect, type Locator, type Page, } from '@playwright/test';
 import { ReactPage } from './ReactPage';
 import * as env from 'dotenv';
 env.config(); 
@@ -10,11 +10,13 @@ export class SpaceDestinationPage extends ReactPage {
 	dropdownplanet: () => Locator;
 	selectLauchbtn: () => Locator;
 	selectplanetbtn: () => Locator;
-	dropdownOptions: (option?: 'Launch' | 'Planet color' | undefined) => Locator;
+	dropdown: (name: 'Launch' | 'Planet color') => Locator;
+	dropdownInput: (name: 'Launch' | 'Planet color') => Locator;
 	launchPickerInput: () => Locator;
 	launchPickerUL: () => Locator;
 	planetPickerInput: () => Locator;
 	planetPickerUL: () => Locator;
+	dropdownOptions: (name: 'Launch' | 'Planet color') => Locator;
 
 	constructor( driver:Page ) {
 		super(driver);
@@ -24,11 +26,44 @@ export class SpaceDestinationPage extends ReactPage {
 		this.selectLauchbtn = () => this.page.locator('[class*="31xyK Gallery__dropdo"]');
 		this.dropdownplanet = () => this.getByReactTool('dropdown' , { hasText: 'Planet color' } );
 		this.selectplanetbtn = () => this.dropdownplanet().getByText('Blue');
-		this.dropdownOptions = (option?: 'Launch' | 'Planet color' | undefined) => this.page.locator('[data-react-toolbox=dropdown]', { hasText: option });
-		this.launchPickerInput = () => this.dropdownOptions('Launch').locator('input');
-		this.launchPickerUL = () => this.dropdownOptions('Launch').locator('ul');
-		this.planetPickerInput = () => this .dropdownOptions('Planet color').locator('input');
-		this.planetPickerUL = () => this.dropdownOptions('Planet color').locator('ul');
+
+		this.dropdown = (name: 'Launch' | 'Planet color') => this.getByReactTool('dropdown', { hasText: name });
+		this.dropdownInput = (name: 'Launch' | 'Planet color') => this.dropdown(name).locator('input');
+		this.dropdownOptions = (name: 'Launch' | 'Planet color') => this.dropdown(name).locator('ul li'); //? Es la estructura de los dropdowns
+
+		this.launchPickerInput = () => this.dropdown('Launch').locator('input');
+		this.launchPickerUL = () => this.dropdown('Launch').locator('ul');
+		this.planetPickerInput = () => this .dropdown('Planet color').locator('input');
+		this.planetPickerUL = () => this.dropdown('Planet color').locator('ul');
+	}
+
+	async openAndGetDropdownOptions(dropdown: 'Launch' | 'Planet color') {
+		await this.dropdownInput(dropdown).click(); // abrir el dropdown.
+		await this.page.waitForTimeout(500); // esperar que la animación termine.
+		const options = this.dropdownOptions(dropdown);
+		return options;
+	}
+
+	async selectDropdownOptionByName(dropdown: 'Launch' | 'Planet color', optionName: string) {
+		const locators = await this.openAndGetDropdownOptions(dropdown);
+		const givenOption = locators.getByText(optionName);
+		const optionValue = await givenOption.textContent(); 
+		if(!optionValue) throw new Error('Option not found');
+		expect.soft(optionValue).toBe(optionName);
+		await givenOption.click(); // seleccionando la opción elegida
+		await this.page.waitForTimeout(500); // esperar que la animación termine.
+		return optionValue;
+	}
+
+	async selectAnyDropdownOption(dropdown: 'Launch' | 'Planet color') {
+		const locators = await this.openAndGetDropdownOptions(dropdown);
+		const options = await locators.all();
+		const randomIndex = Math.floor(Math.random() * options.length);
+		const givenOption = options[randomIndex];
+		const optionValue = await givenOption.textContent();
+		await givenOption.click(); // seleccionando la opción elegida al azar.
+		await this.page.waitForTimeout(500); // esperar que la animación termine.
+		return optionValue;
 	}
 
 	async selectDestination() {

f51ed073df3d234825c9d11f1764336b914cac78
Jhoanna <jhoannafrancia@gmail.com>
2024-05-21 18:10:38 -0300

    GX3-3348: i finish the US i made 1 test case and the test passed i made POM for get more easy the test and for re use the code

--- tests/pages/DemoSliderBarPage.ts
+++ tests/pages/DemoSliderBarPage.ts
@@ -1,14 +1,32 @@
-import type { Locator, Page} from '@playwright/test';
+import type { Locator, Page } from '@playwright/test';
 
 export class DemoSliderBar {
 	page: Page;
-	sliderbarr: () => Locator;
-
+	sliderbar: () => Locator;
 
 	constructor(driver: Page) {
 		this.page = driver; 
-		this.sliderbarr = () => this.page.locator('#sliderContainer')
-		this.sliderbutton = () => this.page.locator('#thumb')
+		this.sliderbar = () => this.page.locator('input[type="range"]');
 
 	}
+	async getSliderValue() {
+		const sliderHandler = this.sliderbar();
+		return await sliderHandler.evaluate((slider) => (slider as HTMLInputElement).value);
+	}
+
+	async moveSliderTo(toLeftRight: number) {
+		const moveSlider = await this.sliderbar();
+		const moveButton = await moveSlider.boundingBox();
+
+		if(moveButton) {
+			const sliderX = moveButton.x + moveButton.width / 2;
+			const sliderY = moveButton.y + moveButton.height / 2; 
+
+			await this.page.mouse.move(sliderX, sliderY);
+			await this.page.mouse.down();
+			await this.page.mouse.move(sliderX + toLeftRight, sliderY);
+			await this.page.mouse.up();
+		}
+	}
+
 }
--- tests/specs/elements/sliderBar_GX3-3348.test.ts
+++ tests/specs/elements/sliderBar_GX3-3348.test.ts
@@ -1,24 +1,14 @@
 import { expect , story, precondition, test } from '@TestBase';
-
 story('GX3-3348 | DemoQA | Slider bar' ,() => {
 	
 	precondition(async ({ page }) => {
 		await page.goto('/slider');
 	});
-	test('move the slider barr', async ({ page }) => {
-		const sliderSelector = page.locator('#sliderContainer');
-		const slider = page.locator('#thumb');
-		
-		const slidermove = await slider.boundingBox();
 
-		if (slidermove) {
-			const moveX = slidermove.x + slidermove.width / 2;
-			const moveY = slidermove.y + slidermove.height / 2; 
-
-			await page.mouse.move(moveX, moveY);
-			await page.mouse.down();
-			await page.mouse.move(moveX + 30, moveY);
-			await page.mouse.up();
-		}
+	test('move slider bar to the right side', async ({ sliderDemo }) => {
+		const firstvalue = await sliderDemo.getSliderValue();
+		await sliderDemo.moveSliderTo(50);
+		const lastValue = await sliderDemo.getSliderValue(); 
+		expect(firstvalue).not.toBe(lastValue);
 	});
 });

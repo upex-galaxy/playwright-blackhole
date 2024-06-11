7a9e70807965b56730b21e7d71a3bf0e0bcfdd70
VickyNovarese <mariavirginianovarese@gmail.com>
2024-06-11 21:07:45 +0200

    test(GX3-3643)-sanityVicky.yml created and button_GX3_3643.test.ts modified

--- tests/specs/elements/button_GX3_3643.test.ts
+++ tests/specs/elements/button_GX3_3643.test.ts
@@ -1,26 +1,36 @@
 import {test} from '@pages/TestBase';
+import { expect } from '@playwright/test';
 
-test.describe('GX3-3643:ToolsQA|Elements|Buttons',()=>{
-
-
-test.beforeEach( async({page})=>{
-	await page.goto('https://demoqa.com/buttons')
-})
-
-
-test('TC1: Validar hacer click en el botón doble click',({page})=>{
-
-	const doubleClickBtn = page.locator('#doubleClickBtn')
-	doubleClickBtn.click()
+		test.describe('GX3-3643:ToolsQA|Elements|Buttons',()=>{
+		test.beforeEach( async({page})=>{
+			await page.goto('https://demoqa.com/buttons', {waitUntil:'domcontentloaded'})
+		})
 
 
-})
+		test('TC1: Validar hacer click en el botón doble click',async({page})=>{
+			const doubleClickBtn = page.locator('#doubleClickBtn')
+			await doubleClickBtn.dblclick()
+			var doubleClickMessage=page.locator('#doubleClickMessage')
+			expect(doubleClickMessage).toBeVisible()
+			expect(doubleClickMessage).toHaveText('You have done a double click')
 
-test('TC2:Realizar click en selector', async({ page })=>{
+		})
 
-	const rightClickBtn = page.locator('#rightClickBtn')
-    rightClickBtn.click() 
+		test('TC2:Realizar click en el botón Right Click', async({ page })=>{
+			const rightClickBtn = page.locator('#rightClickBtn')
+			await rightClickBtn.click({button:'right'})
+			var rightClickMessage=page.locator('#rightClickMessage')
+			expect(rightClickMessage).toBeVisible()
+			expect(rightClickMessage).toHaveText('You have done a right click') 
 
-  })
+		})
+
+		test('TC3:Realizar click en el botón Click', async({ page })=>{
+			const clickBtn = page.getByText('Click Me', {exact:true})
+			await clickBtn.waitFor({state:'visible'})
+			await clickBtn.click();
+			var dynamicClickMessage=page.locator('#dynamicClickMessage')
+			expect(dynamicClickMessage).toHaveText('You have done a dynamic click')
+		})
 
 })

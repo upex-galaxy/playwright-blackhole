7589efcfca2db6408064124c347c2b6019503830
Ivette Castrillon <ivetcastril@gmail.com>
2024-06-18 21:54:45 -0500

    GX3-3746: update test case 4

--- tests/specs/elements/button_GX3-3746.test.ts
+++ tests/specs/elements/button_GX3-3746.test.ts
@@ -45,6 +45,40 @@ story ('GX3-3746: Element Buttons', () => {
 			const messageElement= page.locator('#dynamicClickMessage');
 			await expect (messageElement).toHaveText(expectMessage);
 		});
+	});
+	
+	test ('TC4: should trigger corresponent message by each button ', async ({ page }) => {
+		const expectDoubleClick= await test.step ('Perform Click on Double Click Button', async () => {
+			const expectMessage ='You have done a double click';
+			await page.locator('#doubleClickBtn').dblclick();
+			return expectMessage;
+		});
+		await test.step ('Verify the message', async () => {
+		// *Then (asercion)
+			const messageElement= page.locator('#doubleClickMessage');
+			await expect (messageElement).toHaveText(expectDoubleClick);
+		});
 
+		const expectRightClick= await test.step ('Perform Click on Right Click', async () => {
+			const expectMessage ='You have done a right click';
+			await page.locator('#rightClickBtn').click({ button:'right' });
+			return expectMessage;
+		});
+		await test.step ('Verify the message', async () => {
+		// *Then (asercion)
+			const messageElement= page.locator('#rightClickMessage');
+			await expect (messageElement).toHaveText(expectRightClick);
+		});
+
+		const expectMessage= await test.step ('Perform Click on ClickMessage', async () => {
+			const expectMessage ='You have done a dynamic click';
+			await page.getByText('Click Me',{ exact:true }).click({ button:'left' }); // trabaja con el texto y busca exactitud
+			return expectMessage;
+		});
+		await test.step ('Verify the message', async () => {
+		// *Then (asercion)
+			const messageElement= page.locator('#dynamicClickMessage');
+			await expect (messageElement).toHaveText(expectMessage);
+		});
 	});
 });

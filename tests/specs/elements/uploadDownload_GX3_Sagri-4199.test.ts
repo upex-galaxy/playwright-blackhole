ac1f4be5ebfce9a81fd86c03f87a37c9d04aee6b
Sagri <smanzorro@tipsitpv.com>
2024-07-15 17:55:52 +0200

    test(GX3-4199): Finish test and pipeline for upload and download  On branch GX3-4199_Sagri-Elements/Upload  Your branch is up to date with 'origin/GX3-4199_Sagri-Elements/Upload'.
     Changes to be committed:
    	modified:   .github/workflows/sanitySagri.yml
    	modified:   tests/specs/elements/uploadDownload_GX3_Sagri-4199.test.ts

--- tests/specs/elements/uploadDownload_GX3_Sagri-4199.test.ts
+++ tests/specs/elements/uploadDownload_GX3_Sagri-4199.test.ts
@@ -1,20 +1,20 @@
 import { expect, precondition, story, test } from '@TestBase';
 import { UploadPage } from '@pages/SagriUploadPage';
 
-story('Example Story: Upload and Download', () => {
+story('GX3-4199: ToolsQA | Elements | Upload and Downloa', () => {
 	let uploadPage: UploadPage;
 	precondition(async ({ page }) => {
 		uploadPage = new UploadPage(page);
 		void page.goto('/upload-download', { waitUntil: 'domcontentloaded' });
 	});
 
-	test('TC1: Should download a file', async () => {
+	test('GX3-4200 | TC1: Should download a file', async () => {
 		const { downloadedFiles, downloadedFile } = await uploadPage.downLoadFile();
 		console.log('downloadedFiles:', downloadedFiles);
 		expect(downloadedFiles.includes(downloadedFile)).toBeTruthy();
 	});
 
-	test('TC2: Should upload a file', async () => {
+	test('GX3-4200 | TC2: Should upload a file', async () => {
 		await uploadPage.uploadFile('guapucho.jpg');
 		const value = await uploadPage.uploadFileValue();
 		console.log('uploadedFile Value:', value);
